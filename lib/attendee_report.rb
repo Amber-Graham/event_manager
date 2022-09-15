c7ec880ab3f0e4e6b6ec2d9e18f045e348991841
Amber-Graham <ang190045@gmail.com>
2022-09-15 09:56:37 -0500

    add days and times code

--- lib/attendee_report.rb
+++ lib/attendee_report.rb
@@ -6,17 +6,41 @@ require 'pry-byebug'
 require 'time'
 puts 'Attendee Report initialized'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
-def reghour(regdate)
-  times = [] << DateTime.strptime(regdate, '%D %R').strftime('%H')
+def execute_csv
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
 end
 
-contents.map do |row|
-  regdate = reghour(row[:regdate])
-  puts regdate
+def reghour
+  contents = execute_csv
+  times = []
+  contents.each do |row|
+    regdate = row[:regdate]
+    time = DateTime.strptime(regdate, '%D %R').strftime('%I %p')
+    times << time
+  end
+  freq = times.inject(Hash.new(0)) { |_k,v| _k[v] += 1; _k}
+  sorted_freq = freq.sort_by { |_k, v| v}.reverse.to_h
+  top_times = sorted_freq.keys[0..2]
+  top_times.map { |k,v| "#{k}"}.flatten.join(', ')
 end
+
+def regday
+  contents = execute_csv
+  days = []
+  contents.each do |row|
+    regdate = row[:regdate]
+    day = DateTime.strptime(regdate, '%D %R').strftime('%A')
+    days << day
+  end
+  freq = days.inject(Hash.new(0)) { |k,v| k[v] += 1; k}
+  sorted_freq = freq.sort_by { |k, v| v}.reverse.to_h
+  top_days = sorted_freq.keys[0..2]
+  top_days.map { |k,v| "#{k}"}.flatten.join(', ')
+end
+
+puts "The top three times attendees signed-up for the conference was #{reghour}."
+puts "The top three times attendees signed-up for the conference was #{regday}."
